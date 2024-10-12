b8ba5fb56ee48ab05ce68fd90db76567c7aee728
Malcolm <hauser.malcolm@gmail.com>
2024-10-12 14:09:33 +1000

    Get The Number Of Tables

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,4 +12,17 @@ func getHexString(rawData: Data) -> String {
     return rawData.map { String(format: "%02x", $0) }.joined(separator: " ")
 }
 
-print(getHexString(rawData: try getRawDara(filePath: filePath)))
+/*
+Font Block 1:
+Not Needed uint32 	scaler type 	A tag to indicate the OFA scaler to be used to rasterize this font; see the note on the scaler type below for more information.
+Needed     uint16 	numTables 	    number of tables
+Not Needed uint16 	searchRange 	(maximum power of 2 <= numTables)*16
+Not Needed uint16 	entrySelector 	log2(maximum power of 2 <= numTables)
+Not Needed uint16 	rangeShift 	    numTables*16-searchRange
+*/
+
+
+// Get The Number of Tables And Convet It To Little Endian
+let numTables: UInt16 = try getRawData(filePath: filePath).subdata(in: 4..<6).withUnsafeBytes { $0.load(as: UInt16.self) }.bigEndian
+
+print("numTables: \(numTables)")
