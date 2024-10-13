d851590330af576fe6b91fb780df236c2566ba5e
Malcolm <hauser.malcolm@gmail.com>
2024-10-13 16:42:20 +1000

    Edit the debug Info

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,8 +9,8 @@ for i: UInt16 in 0..<fontHeader.getNumTables() {
     let tag: UInt32 = fontHeader.getTag(id: Int(i))
     let offset: UInt32 = fontHeader.getOffset(id: Int(i))
     
-    print("Tag: \(FileIO().convertUInt32ToASCII(value: tag)) Location: \(offset)")
+    print("Tag: \(FileIO().convertUInt32ToASCII(value: tag)) Offset: \(offset)")
 }
 print("Tables: \(fontHeader.getNumTables())")
-print("Glyf Offset: \(fontHeader.getGlyfOffset())")
-print("End Block 2 Position: \(fontHeader.getEndBlock2Position())")
+print("glyfs Location: \(fontHeader.getGlyfOffset())")
+print("End of Header Location: \(fontHeader.getEndBlock2Position())")
